84d8a8c5b99b319553c6ed4a12f8f8dbf8c235c4
Oscar <oscarliu991110@gmail.com>
2020-01-29 22:59:20 +0000

    Add code for distance and speed using encoder

--- Going_to_bottom_left/Going_to_bottom_left.ino
+++ Going_to_bottom_left/Going_to_bottom_left.ino
@@ -291,3 +291,54 @@ int reliable_ultra_sonic_reading(int pin_num) {
     }
   }
 }
+
+
+double find_distance_encoder(int encoderPin){
+    static int old_value_sensor = 0; //stores old value of sensor reading
+    static int count_encoder = 0; //total count
+    int th_high = 73; //higher threshold of sensor output indicating white strip reached
+    int th_low = 53; //lower threshold of sensor output indicating black strip reached
+    float arc_length = 0.4433; //arc length at radius of wheel in cm (with 72 segments at radius 5.08cm, arc length = 5*(2*pi/72))
+
+    int sensorValue = analogRead(encoderPin);  // variable to store the value coming from the sensor
+    if (sensorValue >= th_high and old_value_sensor < th_high){
+        //checks that old_value is below the higher threshold and that the new value is above the threshold, indicating the sensor has gone from black to white
+        count_encoder ++;
+        old_value_sensor = sensorValue; //sets old_value to the current sensor value ready for the next loop
+    }
+
+
+    else if (sensorValue <= th_low and count_encoder == 0){
+        //checks that the old_value is above the lower threshold and that the new value is below the threshold, indicating the sensor has gone from white to black
+        count_encoder ++;
+        old_value_sensor = sensorValue; //sets old_value to the current sensor value ready for the next loop
+    }
+
+    else if (sensorValue <= th_low and old_value_sensor > th_low){
+        //checks that the old_value is above the lower threshold and that the new value is below the threshold, indicating the sensor has gone from white to black
+        count_encoder ++;
+        old_value_sensor = sensorValue; //sets old_value to the current sensor value ready for the next loop
+    }
+
+
+    double distance = arc_length*count_encoder;
+
+    return distance;
+
+}
+
+
+
+double find_velocity_encoder(int encoderPin){
+
+    double distance1 = find_distance_encoder(encoderPin);
+    int time1 = millis();
+    delay(100);
+    double distance2 = find_distance_encoder(encoderPin);
+    int time2 = millis();
+    double velocity = (distance2-distance1)/(time2-time1);
+
+    return velocity;
+
+
+}
