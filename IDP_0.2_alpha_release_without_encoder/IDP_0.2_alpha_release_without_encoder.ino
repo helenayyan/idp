183574c268eaaf6ac14957a53fcc6ec8e7ff9ea8
Yan Yang <yy452@cam.ac.uk>
2020-02-07 10:08:25 +0000

    0.2 update

--- IDP_0.2_alpha_release_without_encoder/IDP_0.2_alpha_release_without_encoder.ino
+++ IDP_0.2_alpha_release_without_encoder/IDP_0.2_alpha_release_without_encoder.ino
@@ -10,10 +10,20 @@ const int side_ultrasonic_pin = 7;
 const int front_sensorPin = A2;
 const int buttonPin = 3;
 
+const int ledPin =  LED_BUILTIN;// the number of the LED pin
+
+// Variables will change:
+int ledState = LOW;             // ledState used to set the LED
+unsigned long previousMillis = 0;        // will store last time LED was updated
+
+// constants won't change:
+const long interval = 1000;
+
 void setup()
 {
   Serial.begin(9600);
   pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop()
@@ -24,18 +34,29 @@ void loop()
   Adafruit_DCMotor *left = AFMS.getMotor(1);
   Adafruit_DCMotor *right = AFMS.getMotor(2);
   Adafruit_DCMotor *front = AFMS.getMotor(3);
-
+  // blink led
+  unsigned long currentMillis = millis();
+  
   static int victim_num = 0;  //number of victims saved
 
-  if (victim_num = 0)
-
+  while (victim_num < 4)
   {
-    //Going through tunnel
-    int direction_count = 0;  //mark the direction facing to
+    int direction_count = 0;
     bool holding_victum = false;
-    delay(2000);
-    through_tunnel(left, right);
-    direction_count += 1;
+    
+    if (victim_num == 0)
+    {
+      //Going through tunnel from start point
+      delay(2000);
+      through_tunnel(left, right);
+      direction_count = 1;
+    }
+    
+    else if (victim_num > 0 && victim_num < 4) 
+    {
+      red_to_tunnel(left, right);
+      direction_count = 1;
+    }
 
     //Detect whether there is a victim in the front
     clockwise_90(left, right);
@@ -45,31 +66,25 @@ void loop()
     //If there is a victim right in front of the tunnel
     if (distance < 70)
     {
-      delay(3000);
-      //Need to consider why we need to take away 20 seems a lot
-      forward(left, right, distance - 10);
-      delay(5000);
-      gripper_down(front);
+      approach_victim(left, right, front, distance);
       holding_victum = true;
-
     }
 
     //No victim in front of the tunnel start searching left region
     else
     {
-      forward(left, right, 80);
-      backward_wall(left, right, 8);
+      strike_front_wall(left, right);
+      
+      //ensure no victim close to the wall
       distance = reliable_ultra_sonic_reading(side_ultrasonic_pin, side_sensorPin);
       anticlockwise_90(left, right);
       direction_count -= 1;
-
-      if (false)
+      
+      if (distance < 80)
       {
-        //        forward(left, right, distance - 15);
-        //        gripper_down(front);
-        //        holding_victum = true;
+        approach_victim(left, right, front, distance);
+        holding_victum = true;
       }
-
       // No victim along the wall, starting side search
       else
       {
@@ -88,27 +103,23 @@ void loop()
         else
         {
           forward(left, right, 10);
-          backward(left, right, 4);
+          backward_wall(left, right, 5);
         }
       }
     }
-
-    //Going back to the entrance of tunnel
+    //Going back to the middle line of tunnel
     back_to_mark_point(left, right, direction_count);
 
     //If holding a victum, going back to the red region
     if (holding_victum == true)
     {
-      forward(left, right, 155);
-      clockwise_90(left, right);
-      forward(left, right, 60);
-      gripper_up(front);
+      back_to_red(left, right, front);
       victim_num++;
       direction_count = 0;
       delay(5000);
     }
 
-    //If not holding a victim, continue searching
+    //If not holding a victim, continue searching right side
     else
     {
       clockwise_90(left, right);
@@ -116,222 +127,53 @@ void loop()
       direction_count = 1;
 
       //Strike the wall to adjust position
-      forward(left, right, 80);
-      backward_wall(left, right, 8);
+      strike_front_wall(left, right);
 
-      //Detect if there is a victim around the wall
       anticlockwise_90(left, right);
       direction_count--;
 
-      //If a victum is found
-      if (false)
-      {
-        //        forward(left, right, distance - 15);
-        //        gripper_down(front);
-        //        holding_victum = true;
-      }
+      //Adjust position, need to go backward due to position of side sensor
+      backward(left, right, 10);
+      bool side_search_result = side_search_R(left, right);
 
-      //If no vitum is found
+      //If a victim is found
+      if (side_search_result == true)
+      {
+        found_victim_R(left, right, front);
+        direction_count -= 1;
+        holding_victum = true;
+      }
       else
       {
-        //Adjust position, need to go backward due to position of side sensor
-
-        backward(left, right, 10);
-        bool side_search_result = side_search_R(left, right);
-
-        //If a victim is found
-        if (side_search_result == true)
-        {
-          found_victim_R(left, right, front);
-          direction_count -= 1;
-          holding_victum = true;
-        }
-        else
-        {
-          backward(left, right, 15);
-          forward(left, right, 4);
-        }
-      }
-
-      back_to_mark_point(left, right, direction_count);
-
-      if (holding_victum == true)
-      {
-        forward(left, right, 160);
-        clockwise_90(left, right);
-        forward(left, right, 60);
-        gripper_up(front);
-        victim_num++;
-        direction_count = 0;
-        delay(5000);
+        backward(left, right, 15);
+        forward(left, right, 4);
       }
     }
 
-    victim_num++;
-    delay(10000);
-  }
-
-  // ------------------------------------------------------------------------------------------------
-  // New code for 0.2 alpha below
-  if (victim_num > 0 && victim_num < 4)
-  {
-    int direction_count = 0;
-    anticlockwise_90(left, right);
-    direction_count--;
-    anticlockwise_90(left, right);
-    direction_count--;
-    forward(left, right, 70);
-    anticlockwise_90(left, right);
-    direction_count--;
-
-    bool holding_victum = false;
-    forward(left, right, 98);
-
-    //Detect whether there is a victim in the front
-    clockwise_90(left, right);
-    int distance = reliable_ultra_sonic_reading(side_ultrasonic_pin, side_sensorPin);
-    anticlockwise_90(left, right);
-
-    //If there is a victim right in front of the tunnel
-    if (distance < 70)
-    {
-      delay(3000);
-      //Need to consider why we need to take away 20 seems a lot
-      forward(left, right, distance - 10);
-      delay(5000);
-      gripper_down(front);
-      holding_victum = true;
-
-    }
-
-    //No victim in front of the tunnel start searching left region
-    else
-    {
-      forward(left, right, 80);
-      backward_wall(left, right, 8);
-      distance = reliable_ultra_sonic_reading(side_ultrasonic_pin, side_sensorPin);
-      anticlockwise_90(left, right);
-      direction_count -= 1;
-
-      if (false)
-      {
-        //        forward(left, right, distance - 15);
-        //        gripper_down(front);
-        //        holding_victum = true;
-      }
-
-      // No victim along the wall, starting side search
-      else
-      {
-        forward(left, right, 10);
-        bool side_search_result = side_search_L(left, right);
-
-        //If a victim is found
-        if (side_search_result == true)
-        {
-          found_victim_L(left, right, front);
-          direction_count -= 1;
-          holding_victum = true;
-        }
-
-        //If no victim is found
-        else
-        {
-          forward(left, right, 10);
-          backward(left, right, 4);
-        }
-      }
-    }
-
-    //Going back to the entrance of tunnel
     back_to_mark_point(left, right, direction_count);
 
-    //If holding a victum, going back to the red region
     if (holding_victum == true)
     {
-      forward(left, right, 155);
-      clockwise_90(left, right);
-      forward(left, right, 60);
-      gripper_up(front);
+      back_to_red(left, right, front);
       victim_num++;
       direction_count = 0;
       delay(5000);
     }
-
-    //If not holding a victim, continue searching
     else
     {
-      clockwise_90(left, right);
-      clockwise_90(left, right);
-      direction_count = 1;
-
-      //Strike the wall to adjust position
-      forward(left, right, 80);
-      backward_wall(left, right, 8);
-
-      //Detect if there is a victim around the wall
+      //give up and go back to white
+      forward(left, right, 155);
       anticlockwise_90(left, right);
-      direction_count--;
-
-      //If a victum is found
-      if (false)
-      {
-        //        forward(left, right, distance - 15);
-        //        gripper_down(front);
-        //        holding_victum = true;
-      }
-
-      //If no vitum is found
-      else
-      {
-        //Adjust position, need to go backward due to position of side sensor
-
-        backward(left, right, 10);
-        bool side_search_result = side_search_R(left, right);
-
-        //If a victim is found
-        if (side_search_result == true)
-        {
-          found_victim_R(left, right, front);
-          direction_count -= 1;
-          holding_victum = true;
-        }
-        else
-        {
-          backward(left, right, 15);
-          forward(left, right, 4);
-        }
-      }
-
-      back_to_mark_point(left, right, direction_count);
-
-      if (holding_victum == true)
-      {
-        forward(left, right, 160);
-        clockwise_90(left, right);
-        forward(left, right, 60);
-        gripper_up(front);
-        victim_num++;
-        direction_count = 0;
-        delay(5000);
-      }
+      forward(left, right, 96);
+      delay(500000);
     }
-
-    victim_num++;
-    delay(10000);
+    delay(100000);
   }
 
-  if (victim_num == 4)
-  {
-    int direction_count = 0;
-    anticlockwise_90(left, right);
-    direction_count--;
-    anticlockwise_90(left, right);
-    direction_count--;
-    forward(left, right, 170);
-    clockwise_90(left, right);
-    forward(left, right, 20);
-  }
+  //find all four victims
+  backward(left, right, 155);
+  left->run(RELEASE);
+  right->run(RELEASE);
 }
 
 void through_tunnel(Adafruit_DCMotor *left, Adafruit_DCMotor *right)
@@ -547,7 +389,6 @@ int reliable_ultra_sonic_reading(int pin_num, int sensorPin)
     for (int i = 0; i < 5; i++)
     {
       distance[i] = ultra_sonic(pin_num, sensorPin);
-
     }
 
     int max_dist = find_max(distance);
@@ -651,9 +492,7 @@ void found_victim_L(Adafruit_DCMotor *left, Adafruit_DCMotor *right, Adafruit_DC
   int distance = reliable_ultra_sonic_reading(side_ultrasonic_pin, side_sensorPin);
   forward_slowly(left, right, 5);
   anticlockwise_90(left, right);
-  forward(left, right, distance - 10);
-  gripper_down(front);
-  delay(3000);
+  approach_victim(left, right, front, distance);
 }
 
 void found_victim_R(Adafruit_DCMotor *left, Adafruit_DCMotor *right, Adafruit_DCMotor *front)
@@ -661,9 +500,7 @@ void found_victim_R(Adafruit_DCMotor *left, Adafruit_DCMotor *right, Adafruit_DC
   int distance = reliable_ultra_sonic_reading(side_ultrasonic_pin, side_sensorPin);
   backward_slowly(left, right, 5);
   anticlockwise_90(left, right);
-  forward(left, right, distance - 10);
-  gripper_down(front);
-  delay(3000);
+  approach_victim(left, right, front, distance);
 }
 
 void backward_wall(Adafruit_DCMotor *left, Adafruit_DCMotor *right, int distance)
@@ -681,4 +518,38 @@ void backward_wall(Adafruit_DCMotor *left, Adafruit_DCMotor *right, int distance
   left->run(RELEASE);
   right->run(RELEASE);
   delay(2000);
+}
+
+void red_to_tunnel(Adafruit_DCMotor *left, Adafruit_DCMotor *right)
+{
+  //back into cave for more victims
+  backward(left, right, 60);
+  clockwise_90(left, right);
+  forward(left, right, 98);
+}
+
+void approach_victim (Adafruit_DCMotor *left, Adafruit_DCMotor *right, Adafruit_DCMotor *front, int distance)
+{
+  //move toward victim when facing it and upload victim
+  delay(1000);
+  forward(left, right, distance-10);
+  delay(1000);
+  gripper_down(front);
+  delay(1000);
+}
+
+void back_to_red(Adafruit_DCMotor *left, Adafruit_DCMotor *right, Adafruit_DCMotor *front)
+{
+  //after back to mark point, to drop victim to red zone
+  forward(left, right, 155);
+  clockwise_90(left, right);
+  forward(left, right, 60);
+  gripper_up(front);
+}
+
+void strike_front_wall(Adafruit_DCMotor *left, Adafruit_DCMotor *right)
+{
+  //strike the front edge to adjust position
+  forward(left, right, 80);
+  backward_wall(left, right, 8);
 }
